5cd83fbab58e65073f10536018ea6e143b5f41e7
yinka <yinka.olu1@gmail.com>
2020-08-13 15:15:31 -0400

    Update GithubViewModel.kt

--- app/src/main/java/com/yinkaolu/githubapp/viewmodel/GithubViewModel.kt
+++ app/src/main/java/com/yinkaolu/githubapp/viewmodel/GithubViewModel.kt
@@ -1,21 +1,21 @@
 package com.yinkaolu.githubapp.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 import com.yinkaolu.githubapp.data.api.ApiError
 import com.yinkaolu.githubapp.data.model.GithubRepo
 import com.yinkaolu.githubapp.data.model.GithubUser
 import com.yinkaolu.githubapp.data.repository.DefaultGithubRepository
 import com.yinkaolu.githubapp.data.repository.GithubRepository
-import kotlin.concurrent.thread
 
-class GithubViewModel(repository: GithubRepository = DefaultGithubRepository()) {
+class GithubViewModel(repository: GithubRepository = DefaultGithubRepository()): ViewModel() {
     private val repository: GithubRepository = repository
     private var previousUserName = ""
 
-    val user: LiveData<GithubUser>? = repository.currentUser
+    val user: LiveData<GithubUser> = repository.currentUser
     val userApiError: LiveData<ApiError?> = repository.userApiError
 
-    val repos: LiveData<List<GithubRepo>>? = repository.currentRepoList
+    val repos: LiveData<List<GithubRepo>> = repository.currentRepoList
     val repoApiError: LiveData<ApiError?> = repository.repoApiError
 
     fun loadUserData(userName: String, forceDownload: Boolean=false) {
