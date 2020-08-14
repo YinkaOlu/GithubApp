a099de962a8034d1c88dacb17b5824657f2c5e0c
yinka <yinka.olu1@gmail.com>
2020-08-13 23:20:46 -0400

    Add repo bottomsheet

--- app/src/main/java/com/yinkaolu/githubapp/view/fragment/repolist/RepoListFragment.kt
+++ app/src/main/java/com/yinkaolu/githubapp/view/fragment/repolist/RepoListFragment.kt
@@ -5,13 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.yinkaolu.githubapp.R
 import com.yinkaolu.githubapp.data.model.GithubRepo
 import java.lang.IllegalArgumentException
 
-class RepoListFragment : Fragment() {
+class RepoListFragment : Fragment(), RepoListItemListener {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,9 +26,10 @@ class RepoListFragment : Fragment() {
 
         if (repos === null) throw IllegalArgumentException("Repo list fragment requires bundle with ArrayList of GithubRepo")
 
+        val repoListAdapter = RepoListAdapter(repos, this)
         repoList.apply {
             layoutManager = LinearLayoutManager(requireContext())
-            adapter = RepoListAdapter(repos)
+            adapter = repoListAdapter
         }
         return view
     }
@@ -41,5 +44,18 @@ class RepoListFragment : Fragment() {
             frag.arguments = repoBundle
             return frag
         }
+    }
+
+    override fun onSelected(repo: GithubRepo) {
+        val repoBottomSheet = BottomSheetDialog(requireContext())
+        val bottomSheetView = layoutInflater.inflate(R.layout.repo_bottom_sheet, null)
+
+        // Add repo details
+        bottomSheetView.findViewById<TextView>(R.id.repoForks).text = "${repo.forks}"
+        bottomSheetView.findViewById<TextView>(R.id.repoStars).text = "${repo.stargazers}"
+        bottomSheetView.findViewById<TextView>(R.id.repoLastUpdated).text = repo.updatedAt
+
+        repoBottomSheet.setContentView(bottomSheetView)
+        repoBottomSheet.show()
     }
 }
--- app/src/main/java/com/yinkaolu/githubapp/view/fragment/repolist/RepoListItemHolder.kt
+++ app/src/main/java/com/yinkaolu/githubapp/view/fragment/repolist/RepoListItemHolder.kt
@@ -1,17 +1,24 @@
 package com.yinkaolu.githubapp.view.fragment.repolist
 
+import android.content.Context
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.yinkaolu.githubapp.R
 import com.yinkaolu.githubapp.data.model.GithubRepo
 
-class RepoListItemHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    var titleTextView: TextView = itemView.findViewById(R.id.repoTitle)
-    var descTextView: TextView = itemView.findViewById(R.id.repoDesc)
+interface RepoListItemListener {
+    fun onSelected(repo: GithubRepo)
+}
 
-    fun onBind(repo: GithubRepo) {
+class RepoListItemHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    private var titleTextView: TextView = itemView.findViewById(R.id.repoTitle)
+    private var descTextView: TextView = itemView.findViewById(R.id.repoDesc)
+
+    fun onBind(repo: GithubRepo, listener: RepoListItemListener) {
         titleTextView.text = repo.name
         descTextView.text = repo.description
+        itemView.setOnClickListener {listener.onSelected(repo)}
     }
 }
